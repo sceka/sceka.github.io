c68926465aa94c238bf048baf7635ee7d31b9a89
sceka <scekicm02@gmail.com>
2022-07-03 15:33:47 +0200

    Add form for sending emails

---
+++ forms/script.js
@@ -0,0 +1,7 @@
+const nameInput = document.querySelector("#name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const success = document.querySelector("#success");
+const error = document.querySelector("#error");
+
+function validateForm() {}
